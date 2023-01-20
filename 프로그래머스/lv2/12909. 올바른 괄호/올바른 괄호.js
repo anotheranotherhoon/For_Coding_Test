94a947f30df3818b5b50b3d69eb06679a6ebcfb7
Changhoon Lee <changhoon1030@gmail.com>
2023-01-20 23:26:59 +0900

    [level 2] Title: 올바른 괄호, Time: 5.20 ms, Memory: 38.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,9 @@
+function solution(s) {
+  let check = 0;
+  for (let round of s) {
+    round === "(" ? check++ : check--;
+    if (check < 0) return false;
+  }
+
+  return check === 0 ? true : false;
+}
