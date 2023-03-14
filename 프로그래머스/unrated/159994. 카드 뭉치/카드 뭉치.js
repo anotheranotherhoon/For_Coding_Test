49769e79bb4f4cbad79bc034a04e8bf7f3cc7487
Changhoon Lee <changhoon1030@gmail.com>
2023-03-14 09:08:28 +0900

    [unrated] Title: 카드 뭉치, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,12 @@
+function solution(cards1, cards2, goal) {
+    for(let item of goal){
+        if(item===cards1[0]){
+            cards1.shift()
+        }else if(item===cards2[0]){
+            cards2.shift()
+        }else{
+            return "No"
+        }
+    }
+    return "Yes"
+}
